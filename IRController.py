6ff9454e3436dcc6207edbec9651a22ea43dea10
Samuel Vizcaino Paez <samuel.vizcaino@udea.edu.co>
2020-11-19 14:31:10 -0500

    IRPlotter complete implementation almost done

--- IRController.py
+++ IRController.py
@@ -9,6 +9,7 @@ import matplotlib
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 from scipy.signal import argrelextrema
+import math
 
 matplotlib.use('Qt5Agg')
 
@@ -18,7 +19,7 @@ class IRCanvas(FigureCanvasQTAgg):
     docstring
     """
     def __init__(self, parent=None):
-        self.fig = Figure(figsize=(11, 4), dpi=100, tight_layout=True, facecolor='#f8b4a5')
+        self.fig = Figure(figsize=(11, 4), dpi=100, facecolor='#f8b4a5', tight_layout=True)
         self.axes = self.fig.add_subplot(111)
         super(IRCanvas, self).__init__(self.fig)
 
@@ -30,79 +31,183 @@ class IRPlotter(qtw.QWidget):
     def __init__(self, csvData1):
         super(IRPlotter, self).__init__()
         uic.loadUi('Views/uiIRPlotter.ui', self)
-        self.df = pd.read_csv(csvData1, names=('Wavelenght', 'Transmitance'))
-        self.title = csvData1.replace('.csv', '')
-        self.threshold = 0.9975
+        self.df = pd.read_csv(csvData1, names=('Wavenumber', 'Raw Data'))
+        self._title = csvData1.replace('.csv', '')
+        self._baseThreshold = self.uiBaseThresholdSpinBox.value()
+        self._peaksThreshold = self.uiPeaksThresholdSpinBox.value()
+        self._orderBase = self.uiOrderBaseSpinBox.value()
+        self._orderPeaks = self.uiOrderPeaksSpinBox.value()
+        self._showGrid = self.uiShowGridButton.isChecked()
+        self._peaksList = []
         self.canvas = IRCanvas(self)
         self.toolbar = NavigationToolbar(self.canvas, self)
         self.uiToolbarLayout.addWidget(self.toolbar)
         self.uiSpectraLayout.addWidget(self.canvas)
-        self.Plot()
+        self.cid = self.canvas.fig.canvas.mpl_connect('button_press_event', self.PeaksDetectionManual)
+        self.SetYAxis()
+        self.SelectYAxis()
 # ------------------------------------SIGNALS---------------------------------------------------------------
-        self.uiZeroButton.clicked.connect(self.ZeroCorrection)
         self.uiDetectBaselineButton.clicked.connect(self.BaselineDetection)
         self.uiSubsBaselineButton.clicked.connect(self.BaselineSubs)
-        self.uiThresholdSpinBox.valueChanged.connect(self.SetThreshold)
+        self.uiBaseThresholdSpinBox.valueChanged.connect(self.SetBaseThreshold)
+        self.uiPeaksThresholdSpinBox.valueChanged.connect(self.SetPeaksThreshold)
+        self.uiOrderBaseSpinBox.valueChanged.connect(self.SetOrderBase)
+        self.uiOrderPeaksSpinBox.valueChanged.connect(self.SetOrderPeaks)
+        self.uiShowGridButton.clicked.connect(self.SetGrid)
+        self.uiTransButton.clicked.connect(self.SelectYAxis)
+        self.uiPercentTransButton.clicked.connect(self.SelectYAxis)
+        self.uiAbsorbanceButton.clicked.connect(self.SelectYAxis)
+        self.uiRestoreButton.clicked.connect(self.Restore)
+        self.uiPeakAutoButton.clicked.connect(self.PeaksDetectionAuto)
 # ------------------------------------METHODS---------------------------------------------------------------
 
-    def Plot(self, yList='Transmitance', showGrid=True):
+    def Plot(self, yList):
         """
         docstring
         """
         self.canvas.axes.clear()
-        self.canvas.axes.plot(self.df['Wavelenght'], self.df[yList])
-        self.canvas.axes.set_title(self.title)
-        self.canvas.axes.set_xlabel('Wavelenght [cm-1]')
-        self.canvas.axes.set_ylabel('Transmitance')
-        self.canvas.axes.set_xlim(4000, 1000)
-        self.canvas.axes.grid(True)
+        self.canvas.axes.plot(self.df['Wavenumber'], self.df[yList], linewidth=1.0)
+        self.canvas.axes.set_title(self._title)
+        self.canvas.axes.set_xlabel('Wavenumber [cm-1]')
+        self.canvas.axes.set_ylabel(yList[0]) if type(yList) == list else self.canvas.axes.set_ylabel(yList)
+        self.canvas.axes.set_xlim(4000, 500)
+        self.canvas.axes.grid(True, color='gray', linestyle=':', linewidth=0.5) if self._showGrid else self.canvas.axes.grid(False)
+        if len(self._peaksList) > 0:
+            if yList == 'Transmittance':
+                intensity = 1
+            elif yList == '%Transmittance':
+                intensity = 2
+            elif yList == 'Absorbance':
+                intensity = 3
+            else:  # quitar
+                intensity = 1
+            for label in self._peaksList:
+                self.canvas.axes.annotate(
+                    str(round(label[0], 0)), xy=(label[0], label[intensity]), xytext=(0, -50),
+                    textcoords='offset points', ha='center', va='bottom', rotation=90,
+                    bbox=dict(boxstyle='round', fc='white', alpha=0.5),
+                    arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0')
+                )
         self.canvas.draw()
 
-    def SetThreshold(self):
-        """
-        docstring
-        """
-        self.threshold = float(self.uiThresholdSpinBox.value())
+    def SetBaseThreshold(self):
+        self._baseThreshold = self.uiBaseThresholdSpinBox.value()
+        if self.df.get('baseline') is not None:
+            self.BaselineDetection()
 
-    def ZeroCorrection(self):
+    def SetPeaksThreshold(self):
+        self._peaksThreshold = self.uiPeaksThresholdSpinBox.value()
+        if self.df.get('peaks') is not None:
+            del self.df['peaks']
+            self.PeaksDetectionAuto()
+
+    def SetOrderBase(self):
+        self._orderBase = self.uiOrderBaseSpinBox.value()
+        if self.df.get('baseline') is not None:
+            self.BaselineDetection()
+
+    def SetOrderPeaks(self):
+        self._orderPeaks = self.uiOrderPeaksSpinBox.value()
+        if self.df.get('peaks') is not None:
+            self.PeaksDetectionAuto()
+
+    def SetGrid(self):
+        self._showGrid = self.uiShowGridButton.isChecked()
+        self.SelectYAxis()
+
+    def SetYAxis(self):
         """
         docstring
         """
-        maxValue = self.df['Transmitance'].max()
-        zeroCorrection = 1 - maxValue
-        self.df['Transmitance'] = self.df['Transmitance'] + zeroCorrection
-        self.Plot()
+        if 0.0 < self.df['Raw Data'].median() < 1.0:
+            self.df['Transmittance'] = self.df['Raw Data']
+        self.df['%Transmittance'] = self.df['Transmittance'] * 100
+        self.df['Absorbance'] = pd.Series([2.0 - math.log10(i) for i in self.df['%Transmittance']])
+
+    def SelectYAxis(self):
+        if self.uiTransButton.isChecked():
+            self.uiPercentTransButton.setChecked(False)
+            self.uiAbsorbanceButton.setChecked(False)
+            self.Plot('Transmittance')
+        elif self.uiPercentTransButton.isChecked():
+            self.uiAbsorbanceButton.setChecked(False)
+            self.uiTransButton.setChecked(False)
+            self.Plot('%Transmittance')
+        elif self.uiAbsorbanceButton.isChecked():
+            self.uiPercentTransButton.setChecked(False)
+            self.uiTransButton.setChecked(False)
+            self.Plot('Absorbance')
 
     def BaselineDetection(self):
         """
         docstring
         """
-        orderBase = 50
-        self.df['baseline'] = self.df.loc[argrelextrema(self.df.Transmitance.values, np.greater_equal, order=orderBase)[0], 'Transmitance']
-        mask1 = (self.df['baseline'] < self.threshold)
+        self.df['baseline'] = self.df.loc[argrelextrema(self.df.Transmittance.values, np.greater_equal, order=self._orderBase)[0], 'Transmittance']
+        mask1 = (self.df['baseline'] < self._baseThreshold)
         self.df.loc[mask1, 'baseline'] = np.nan
 
         mask2 = (self.df['baseline'].isnull() == False)
-        baselineX = self.df.loc[mask2, 'Wavelenght']
-        baselineY = self.df.loc[mask2, 'Transmitance']
+        baselineX = self.df.loc[mask2, 'Wavenumber']
+        baselineY = self.df.loc[mask2, 'Transmittance']
         coef = np.polyfit(baselineX, baselineY, 1)
         self.baselineFit = np.poly1d(coef)
-        self.df['baseline'] = pd.Series([self.baselineFit(i) for i in self.df['Wavelenght']])
-        self.Plot(yList=['Transmitance', 'baseline'])
+        self.df['baseline'] = pd.Series([self.baselineFit(i) for i in self.df['Wavenumber']])
+        self.Plot(['Transmittance', 'baseline'])
 
     def BaselineSubs(self):
         """
         docstring
         """
-        self.df['Transmitance'] = 1 - self.df['baseline'] + self.df['Transmitance']
-        self.Plot()
+        self.df['Transmittance'] = 1 - self.df['baseline'] + self.df['Transmittance']
+        self.df['%Transmittance'] = self.df['Transmittance'] * 100
+        self.df['Absorbance'] = pd.Series([2.0 - math.log10(i) for i in self.df['%Transmittance']])
+        self.uiDetectBaselineButton.setEnabled(False)
+        self.uiSubsBaselineButton.setEnabled(False)
+        self.uiBaseThresholdSpinBox.setEnabled(False)
+        self.uiOrderBaseSpinBox.setEnabled(False)
+        self.SelectYAxis()
 
-    def PeakDetection(self):
+    def Restore(self):
         """
         docstring
         """
-        orderPeaks = 20
-        self.df['peaks'] = self.df.loc[argrelextrema(self.df.Transmitance.values, np.less_equal, order=orderPeaks)[0], 'Transmitance']
-        mask3 = (self.df['peaks'] > self.threshold)
+        self.uiDetectBaselineButton.setEnabled(True)
+        self.uiSubsBaselineButton.setEnabled(True)
+        self.uiBaseThresholdSpinBox.setEnabled(True)
+        self.uiOrderBaseSpinBox.setEnabled(True)
+        self.uiPeakManualButton.setChecked(False)
+        if self.df.get('peaks') is not None:
+            del self.df['peaks']
+        self._peaksList = []
+        self.SetYAxis()
+        self.SelectYAxis()
+
+    def PeaksDetectionAuto(self):
+        """
+        docstring
+        """
+        self.df['peaks'] = self.df.loc[argrelextrema(self.df.Transmittance.values, np.less_equal, order=self._orderPeaks)[0], 'Transmittance']
+        mask3 = (self.df['peaks'] > self._peaksThreshold)
         self.df.loc[mask3, 'peaks'] = np.nan
-        self.Plot()
+        mask4 = (self.df['peaks'].isnull() == False)
+        self._peaksList = [
+            (i, j, k, l) for i, j, k, l in zip(
+                self.df.loc[mask4, 'Wavenumber'], self.df.loc[mask4, 'Transmittance'],
+                self.df.loc[mask4, '%Transmittance'], self.df.loc[mask4, 'Absorbance']
+            )
+        ]
+        self.SelectYAxis()
+
+    def PeaksDetectionManual(self, event):
+        """
+        docstring
+        """
+        if self.uiPeakManualButton.isChecked():
+            ix, iy = event.xdata, event.ydata
+            if self.uiTransButton.isChecked():
+                self._peaksList.append((ix, iy, iy * 100, 2.0 - math.log10(iy * 100)))
+            elif self.uiPercentTransButton.isChecked():
+                self._peaksList.append((ix, iy / 100, iy, 2.0 - math.log10(iy)))
+            elif self.uiAbsorbanceButton.isChecked():
+                self._peaksList.append((ix, (10 ** (2 - iy)) / 100, 10 ** (2 - iy), iy))
+        self.SelectYAxis()
--- test.py
+++ test.py
@@ -14,62 +14,29 @@ from scipy.signal import argrelextrema
 matplotlib.use('Qt5Agg')
 
 
-class IRCanvas(FigureCanvasQTAgg):
-    """
-    docstring
-    """
-    def __init__(self, width=5, height=4, dpi=200):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(IRCanvas, self).__init__(fig)
-        
-
-        # 1 - baselineFit(self.df['Wavelenght']) + self.df['Transmitance'] if baselineFit(self.df['Wavelenght']) > self.df['Transmitance'] else self.df['Transmitance']
-        plt.scatter(self.df['Wavelenght'], self.df['min'], c='r')
-        plt.scatter(baselineX, baselineY, c='k')
-        plt.plot(self.df['Wavelenght'], self.df['Transmitance'], label=label)
-        plt.xlim(4000, 1000)
-        plt.xlabel('Wavelenght')
-        plt.ylabel('Transmitance')
-        plt.title('IR')
-        plt.legend()
-        plt.show()
+from matplotlib import pyplot as plt
 
 
-class IRPlotter(qtw.QWidget):
-    """
-    docstring
-    """
-    def __init__(self, csvData1, threshold=0.9975, orderMin=20, orderMax=50):
-        """
-        docstring
-        """
-        label = csvData1.replace('.csv', '')
-        self.df = pd.read_csv(csvData1, names=('Wavelenght', 'Transmitance'))
-        # zero correction
-        maxValue = self.df['Transmitance'].max()
-        zeroCorrection = 1 - maxValue
-        self.df['Transmitance'] = self.df['Transmitance'] + zeroCorrection
-        # baseline detection
-        self.df['max'] = self.df.loc[argrelextrema(self.df.Transmitance.values, np.greater_equal, order=orderMax)[0], 'Transmitance']
-        mask1 = (self.df['max'] < threshold)
-        self.df.loc[mask1, 'max'] = np.nan
+class LineBuilder:
+    def __init__(self, line):
+        self.line = line
+        self.xs = list(line.get_xdata())
+        self.ys = list(line.get_ydata())
+        self.cid = line.figure.canvas.mpl_connect('button_press_event', self)
 
-        mask2 = (self.df['max'].isnull() == False)
-        baselineX = self.df.loc[mask2, 'Wavelenght']
-        baselineY = self.df.loc[mask2, 'Transmitance']
+    def __call__(self, event):
+        print('click', event)
+        if event.inaxes!=self.line.axes: return
+        self.xs.append(event.xdata)
+        self.ys.append(event.ydata)
+        self.line.set_data(self.xs, self.ys)
+        self.line.figure.canvas.draw()
 
-        coef = np.polyfit(baselineX, baselineY, 1)
-        self.baselineFit = np.poly1d(coef)
-        self.df['Transmitance'] = pd.Series([1 - self.baselineFit(i) + i for i in self.df['Transmitance']])
 
-        self.df['min'] = self.df.loc[argrelextrema(self.df.Transmitance.values, np.less_equal, order=orderMin)[0], 'Transmitance']
-        self.df['max'] = self.df.loc[argrelextrema(self.df.Transmitance.values, np.greater_equal, order=orderMax)[0], 'Transmitance']
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('click to build line segments')
+line, = ax.plot([0], [0])  # empty line
+linebuilder = LineBuilder(line)
 
-        mask3 = (self.df['min'] > threshold)
-        self.df.loc[mask3, 'min'] = np.nan
-
-        canvas = IRCanvas(self, width=5, height=4, dpi=100)
-        self.df.plot(ax=canvas.axes)
-        self.setCentralWidget(canvas)
-        self.show()
+plt.show()
